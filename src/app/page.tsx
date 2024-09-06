af4d27f51e0f07ee23a3016ed1fbe9999506a5aa
Black Byte <blackbyte9@gmx.net>
2024-09-06 22:50:07 +0200

    Font and first Nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import NavLinks from '@/ui/nav';
 
 export default function Home() {
   return (
+    <>
+    <NavLinks />
     <main>
       <Container>
         <Row>
-          <Col>&nbsp;</Col>
+          <Col></Col>          
           <Col xs={6}>Main Page</Col>
           <Col>&nbsp;</Col>
         </Row>
       </Container>
     </main>
+    </>
   );
 }
