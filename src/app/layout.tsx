b86f74149cdcf329ec58cca1d7844edc3d238e52
Black Byte <blackbyte9@gmx.net>
2024-09-06 11:39:26 +0200

    cleanup design files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "LFS Vöhringen",
@@ -26,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de" data-bs-theme="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
