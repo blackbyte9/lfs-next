3417be0643e94e2c9094520ef4461ffc68c21b01
Black Byte <blackbyte9@gmx.net>
2024-09-09 21:13:19 +0200

    Fix git chaos

--- src/app/lfb/layout.tsx
+++ src/app/lfb/layout.tsx
@@ -1,8 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import NavLinks from '@/ui/nav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <div >
-        {children}
-      </div>
-    );
-  }
+  return (
+    <>
+      <NavLinks />
+      <main>
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col xs={6}>{children}</Col>
+            <Col>&nbsp;</Col>
+          </Row>
+        </Container>
+      </main>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import NavLinks from '@/ui/nav';
 
 export default function Home() {
   return (
     <>
-    <NavLinks />
     <main>
       <Container>
         <Row>
